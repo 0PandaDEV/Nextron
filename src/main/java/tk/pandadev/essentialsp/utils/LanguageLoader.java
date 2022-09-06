fde520bb06729db1599abe404668dc7a0444550c
PandaDΞV <nils.ulmann@klangkonzept.ch>
2022-09-06 21:56:01 +0200

    fixed language files sync

--- src/main/java/tk/pandadev/essentialsp/utils/LanguageLoader.java
+++ src/main/java/tk/pandadev/essentialsp/utils/LanguageLoader.java
@@ -1,42 +1,49 @@
 package tk.pandadev.essentialsp.utils;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import tk.pandadev.essentialsp.Main;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.HashMap;
 
 public class LanguageLoader {
 
-    HashMap<String, String> translationMap;
+    public static HashMap<String, String> translationMap = new HashMap<String, String>();
+    private File en;
 
     public LanguageLoader(Main plugin){
         File languageDirectory = new File(plugin.getDataFolder(), "languages/");
         File defaultLanguageFile = new File(plugin.getDataFolder(), "languages/en_US.yml");
-        if (!languageDirectory.isDirectory()){
+
+        if (!languageDirectory.exists()){
             languageDirectory.mkdir();
-            try {
-                InputStream stream = plugin.getResource("en_US.yml");
-                FileUtils.copyInputStreamToFile(stream, defaultLanguageFile);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        }
+
+        Main.getInstance().saveResource("languages/en_US.yml", true);
+        Main.getInstance().saveResource("languages/de_DE.yml", true);
+
+        if (plugin.getConfig().getString("locale").equals("en_US")){
+            FileConfiguration translations = YamlConfiguration.loadConfiguration(defaultLanguageFile);
+            for (String translation : translations.getKeys(false)){
+                translationMap.put(translation, translations.getString(translation));
             }
-            if (plugin.getConfig().getString("locale") != null && plugin.getConfig().getString("locale").equals("en_US")) {
-                FileConfiguration translations = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "languages/" + plugin.getConfig().getString("locale") + ".yml"));
-                for (String translation : translations.getKeys(false)){
-                    translationMap.put(translation, translations.getString(translation));
-                }
-            } else {
-                FileConfiguration translations = YamlConfiguration.loadConfiguration(defaultLanguageFile);
-                for (String translation : translations.getKeys(false)){
-                    translationMap.put(translation, translations.getString(translation));
-                }
+        } else {
+            FileConfiguration translations = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "languages/" + plugin.getConfig().getString("locale") + ".yml"));
+            for (String translation : translations.getKeys(false)){
+                translationMap.put(translation, translations.getString(translation));
             }
         }
 
     }
+
+    public void copyToFile(InputStream inputStream, File file) throws IOException {
+        try(OutputStream outputStream = Files.newOutputStream(file.toPath())) {
+            IOUtils.copy(inputStream, outputStream);
+        }
+    }
+
 }
