a917589630c722e10fec551f78607af54b190cc1
PandaDΞV <nils.ulmann@klangkonzept.ch>
2022-09-10 13:14:56 +0200

    fixed speed command bugs

--- src/main/java/tk/pandadev/essentialsp/commands/SpeedCommand.java
+++ src/main/java/tk/pandadev/essentialsp/commands/SpeedCommand.java
@@ -1,5 +1,6 @@
 package tk.pandadev.essentialsp.commands;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,28 +21,44 @@ public class SpeedCommand implements CommandExecutor {
 
         if (args.length == 1){
             if (player.hasPermission("essentialsp.speed")){
-                float speed = Float.parseFloat(args[0]) / 10f;
-                if (speed > 1) {
-                    player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_error"));
-                } else {
-                    if (player.isFlying()){
-                        player.setAllowFlight(true);
-                        player.setFlying(true);
-                        player.setFlySpeed(speed);
-                        if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")){
-                            player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_fly_success").replace("%s", args[0]));
-                        }
-                    }else if (player.isOnGround()){
-                        player.setWalkSpeed(speed);
-                        if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")){
-                            player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_walk_success").replace("%s", args[0]));
+                if (NumberUtils.isNumber(args[0])) {
+                    float speed = Integer.parseInt(args[0]) / 10f;
+                    if (speed > 1) {
+                        player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_error"));
+                    } else {
+                        if (player.isFlying()) {
+                            player.setAllowFlight(true);
+                            player.setFlying(true);
+                            player.setFlySpeed(speed);
+                            if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")) {
+                                player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_fly_success").replace("%s", args[0]));
+                            }
+                        } else if (player.isOnGround()) {
+                            player.setWalkSpeed(speed);
+                            if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")) {
+                                player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_walk_success").replace("%s", args[0]));
+                            }
                         }
                     }
+                } else {
+                    player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_error_digit"));
                 }
             } else{
                 player.sendMessage(Main.getNoPerm());
             }
-        } else {
+        } else if (args.length == 0){
+            if (player.hasPermission("essentialsp.speed.reset")){
+                player.setAllowFlight(true);
+                player.setFlying(true);
+                player.setFlySpeed((float) 0.1);
+                player.setWalkSpeed((float) 0.2);
+                player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("speed_reset"));
+            }else{
+                player.sendMessage(Main.getNoPerm());
+            }
+        }
+
+        else {
             player.sendMessage(Main.getPrefix() + "§c/speed <amount>");
         }
 
