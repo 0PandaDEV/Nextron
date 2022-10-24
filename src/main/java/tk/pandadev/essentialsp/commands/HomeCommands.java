8deb5f175e29e95963ea627dd8a2a16bb9c259c4
PandaDEV <nils.ulmann@klangkonzept.ch>
2022-10-24 13:33:51 +0200

    rewrite home command and bug fixes

--- src/main/java/tk/pandadev/essentialsp/commands/HomeCommands.java
+++ src/main/java/tk/pandadev/essentialsp/commands/HomeCommands.java
@@ -1,20 +1,26 @@
 package tk.pandadev.essentialsp.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import tk.pandadev.essentialsp.Main;
+import tk.pandadev.essentialsp.utils.Configs;
 import tk.pandadev.essentialsp.utils.LanguageLoader;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 public class HomeCommands implements CommandExecutor, TabCompleter {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -25,66 +31,48 @@ public class HomeCommands implements CommandExecutor, TabCompleter {
 
         Player player = (Player) (sender);
 
-
         if (label.equalsIgnoreCase("sethome") && args.length == 1){
 
-            if (Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) == null){
-
-                Main.getInstance().getConfig().set("Homes." + player.getUniqueId() + "." + args[0].toLowerCase(), player.getLocation());
-                Main.getInstance().saveConfig();
+            if (Configs.home.getString("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) == null){
+                Configs.home.set("Homes." + player.getUniqueId() + "." + args[0].toLowerCase(), player.getLocation());
+                Configs.saveHomeConfig();
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("sethome_success").replace("%h", args[0].toLowerCase()));
-                return true;
-
             } else {
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("sethome_error").replace("%h", args[0].toLowerCase()));
             }
 
         } else if (label.equalsIgnoreCase("sethome") && args.length == 0){
 
-            Main.getInstance().getConfig().set("Homes." + player.getUniqueId() + ".default", player.getLocation());
-            Main.getInstance().saveConfig();
+            Configs.home.set("Homes." + player.getUniqueId() + ".default", player.getLocation());
+            Configs.saveHomeConfig();
             player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("sethome_default_success"));
-            return true;
 
         } else if (label.equalsIgnoreCase("delhome") && args.length == 1){
 
-            if (Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) != null){
-
-                Main.getInstance().getConfig().set("Homes." + player.getUniqueId() + "." + args[0].toLowerCase(), null);
-                Main.getInstance().saveConfig();
+            if (Configs.home.getString("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) != null){
+                Configs.home.set("Homes." + player.getUniqueId() + "." + args[0].toLowerCase(), null);
+                Configs.saveHomeConfig();
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("delhome_success").replace("%h", args[0].toLowerCase()));
-                return true;
-
             } else {
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("delhome_error").replace("%h", args[0].toLowerCase()));
             }
 
         } else if (label.equalsIgnoreCase("home") && args.length == 1){
 
-            if (Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) != null){
-
-                player.teleport((Location) Objects.requireNonNull(Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + "." + args[0].toLowerCase())));
-                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
-                if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")){
-                    player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_success").replace("%h", args[0].toLowerCase()));
-                }
-                return true;
-
+            if (Configs.home.getString("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()) != null){
+                player.teleport((Location) Configs.home.get("Homes." + player.getUniqueId() + "." + args[0].toLowerCase()));
+                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 100, 1);
+                player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_success").replace("%h", args[0].toLowerCase()));
             } else {
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_error").replace("%h", args[0].toLowerCase()));
             }
 
         } else if (label.equalsIgnoreCase("home") && args.length == 0){
 
-            if (Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + ".default") != null){
-
-                player.teleport((Location) Objects.requireNonNull(Main.getInstance().getConfig().get("Homes." + player.getUniqueId() + ".default")));
-                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
-                if (Main.getInstance().getSettingsConfig().getBoolean(player.getUniqueId() + ".feedback")){
-                    player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_default_success"));
-                }
-                return true;
-
+            if (Configs.home.getString("Homes." + player.getUniqueId() + ".default") != null){
+                player.teleport((Location) Configs.home.get("Homes." + player.getUniqueId() + ".default"));
+                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 100, 1);
+                player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_default_success"));
             } else {
                 player.sendMessage(Main.getPrefix() + LanguageLoader.translationMap.get("home_default_error"));
             }
@@ -102,8 +90,14 @@ public class HomeCommands implements CommandExecutor, TabCompleter {
         ArrayList<String> list = new ArrayList<String>();
         Player playert = (Player)(sender);
 
-        if (args.length == 1 && label.equalsIgnoreCase("home") || label.equalsIgnoreCase("delhome")) {
-            list.addAll(Objects.requireNonNull(Main.getInstance().getConfig().getConfigurationSection("Homes." + playert.getUniqueId())).getKeys(false));
+        if (Configs.home.getConfigurationSection("Homes") == null){
+            System.out.println("null in homes");
+        } else if (Configs.home.getConfigurationSection("Homes").getKeys(false).isEmpty()){
+            System.out.println("empty in homes");
+        } else {
+            if (args.length == 1 && label.equalsIgnoreCase("home") || label.equalsIgnoreCase("delhome")) {
+                list.addAll(Objects.requireNonNull(Configs.home.getConfigurationSection("Homes." + playert.getUniqueId())).getKeys(false));
+            }
         }
 
         ArrayList<String> completerList = new ArrayList<String>();
