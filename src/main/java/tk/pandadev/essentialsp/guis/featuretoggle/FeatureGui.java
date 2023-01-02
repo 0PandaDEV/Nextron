4b1b6042cdeec760b7f0b4f812e399303ac82616
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-01-02 23:06:11 +0100

    added home and tpa switcher

--- src/main/java/tk/pandadev/essentialsp/guis/featuretoggle/FeatureGui.java
+++ src/main/java/tk/pandadev/essentialsp/guis/featuretoggle/FeatureGui.java
@@ -2,8 +2,10 @@ package tk.pandadev.essentialsp.guis.featuretoggle;
 
 import games.negative.framework.gui.GUI;
 import games.negative.framework.util.ItemBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.permissions.PermissionAttachment;
 import tk.pandadev.essentialsp.Main;
@@ -30,16 +32,79 @@ public class FeatureGui extends GUI {
         ItemStack off_warp = new ItemBuilder(Material.RECOVERY_COMPASS)
                 .setName("§c❌ §8• §7Warp System")
                 .addLoreLine("")
-                .addLoreLine("§8The warp system can't be used by normal players now!")
+                .addLoreLine(LanguageLoader.translationMap.get("featuregui_warp_on"))
                 .addLoreLine("")
                 .addLoreLine(LanguageLoader.translationMap.get("leftclick"))
                 .build();
 
-        setItemClickEvent(13, player -> Configs.feature.getBoolean("warp_system") ? on_warp : off_warp, (player, event) -> {
+        setItemClickEvent(21, player -> Configs.feature.getBoolean("warp_system") ? on_warp : off_warp, (player, event) -> {
             Configs.feature.set("warp_system", !Configs.feature.getBoolean("warp_system"));
             Configs.saveFeatureConfig();
-            player.addAttachment(Main.getInstance()).setPermission("essentialsp.warp", Configs.feature.getBoolean("warp_system"));
-            player.playSound(player.getLocation(), Configs.feature.getBoolean("warp_system") ? Sound.BLOCK_BEACON_DEACTIVATE : Sound.BLOCK_BEACON_ACTIVATE, 100, 1);
+            for (Player onlineplayer : Bukkit.getOnlinePlayers()){
+                onlineplayer.addAttachment(Main.getInstance()).setPermission("essentialsp.warp", Configs.feature.getBoolean("warp_system"));
+            }
+            player.playSound(player.getLocation(), Configs.feature.getBoolean("warp_system") ? Sound.BLOCK_BEACON_ACTIVATE : Sound.BLOCK_BEACON_DEACTIVATE, 100, 1);
+            new FeatureGui().open(player);
+        });
+
+        //////////////////////////////////////////////////////////////////
+
+        ///////////////////////// Home System ////////////////////////////
+
+        ItemStack on_home = new ItemBuilder(Material.COMPASS)
+                .setName("§a✔ §8• §7Home System")
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("featuregui_home_off"))
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("leftclick"))
+                .build();
+
+        ItemStack off_home = new ItemBuilder(Material.COMPASS)
+                .setName("§c❌ §8• §7Home System")
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("featuregui_home_on"))
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("leftclick"))
+                .build();
+
+        setItemClickEvent(23, player -> Configs.feature.getBoolean("home_system") ? on_home : off_home, (player, event) -> {
+            Configs.feature.set("home_system", !Configs.feature.getBoolean("home_system"));
+            Configs.saveFeatureConfig();
+            for (Player onlineplayer : Bukkit.getOnlinePlayers()){
+                onlineplayer.addAttachment(Main.getInstance()).setPermission("essentialsp.home", Configs.feature.getBoolean("home_system"));
+            }
+            player.playSound(player.getLocation(), Configs.feature.getBoolean("home_system") ? Sound.BLOCK_BEACON_ACTIVATE : Sound.BLOCK_BEACON_DEACTIVATE, 100, 1);
+            new FeatureGui().open(player);
+        });
+
+        //////////////////////////////////////////////////////////////////
+
+        ///////////////////////// Tpa System /////////////////////////////
+
+        ItemStack on_tpa = new ItemBuilder(Material.NAME_TAG)
+                .setName("§a✔ §8• §7Tpa System")
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("featuregui_tpa_off"))
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("leftclick"))
+                .build();
+
+        ItemStack off_tpa = new ItemBuilder(Material.NAME_TAG)
+                .setName("§c❌ §8• §7Tpa System")
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("featuregui_tpa_on"))
+                .addLoreLine("")
+                .addLoreLine(LanguageLoader.translationMap.get("leftclick"))
+                .build();
+
+        setItemClickEvent(25, player -> Configs.feature.getBoolean("tpa_system") ? on_tpa : off_tpa, (player, event) -> {
+            Configs.feature.set("tpa_system", !Configs.feature.getBoolean("tpa_system"));
+            Configs.saveFeatureConfig();
+            for (Player onlineplayer : Bukkit.getOnlinePlayers()){
+                onlineplayer.addAttachment(Main.getInstance()).setPermission("essentialsp.tpa", Configs.feature.getBoolean("tpa_system"));
+                onlineplayer.addAttachment(Main.getInstance()).setPermission("essentialsp.tpaccept", Configs.feature.getBoolean("tpa_system"));
+            }
+            player.playSound(player.getLocation(), Configs.feature.getBoolean("tpa_system") ? Sound.BLOCK_BEACON_ACTIVATE : Sound.BLOCK_BEACON_DEACTIVATE, 100, 1);
             new FeatureGui().open(player);
         });
 
