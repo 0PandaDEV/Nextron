bb1149204943526061f46564540442bf22b861a6
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-01-12 22:24:07 +0100

    added plugin creator message when joining a server

--- src/main/java/tk/pandadev/essentialsp/listeners/JoinListener.java
+++ src/main/java/tk/pandadev/essentialsp/listeners/JoinListener.java
@@ -1,6 +1,8 @@
 package tk.pandadev.essentialsp.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +23,12 @@ public class JoinListener implements Listener {
             event.setJoinMessage("");
         }else {
             event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("join_message").replace("%p", player.getName())));
+        }
+        if (player.getUniqueId().equals("2dae5251-257a-4d28-b220-60fe24de72f0")){
+            event.setJoinMessage(event.getJoinMessage() + " §8• §x§6§2§0§0§f§fPlugin Creator");
+            for (Player onlineplayer : Bukkit.getOnlinePlayers()){
+                onlineplayer.playSound(onlineplayer.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 100, 1);
+            }
         }
         RankAPI.createPlayerTeam(player);
         RankAPI.checkRank(player);
