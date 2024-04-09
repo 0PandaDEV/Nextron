5d91054138898bdb43f89d4dc45c563a5de132b4
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-04-09 19:52:02 +0200

    ported sudo command

---
+++ src/main/java/net/pandadev/nextron/commands/SudoCommand.java
@@ -0,0 +1,23 @@
+package net.pandadev.nextron.commands;
+
+import ch.hekates.languify.language.Text;
+import net.pandadev.nextron.Main;
+import net.pandadev.nextron.utils.commandapi.Command;
+import net.pandadev.nextron.utils.commandapi.paramter.Param;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SudoCommand extends HelpBase {
+
+    public SudoCommand() {
+        super("sudo", "Forces a player to execute a command", "/sudo <player> <command>");
+    }
+
+    @Command(names = "sudo", permission = "nextron.sudo")
+    public void sudoCommand(CommandSender sender, @Param(name = "target") Player target, @Param(name = "command", concated = true) String command) {
+        target.chat("/" + command);
+
+        sender.sendMessage(Main.getPrefix()
+                + Text.get("sudo.success").replace("%t", target.getName()).replace("%b", command));
+    }
+}
