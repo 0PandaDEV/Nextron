d9e51b4e9b577d97fd6eed8dc3499b7005a34e1d
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-04-28 23:37:48 +0200

    fixed error messages and added a usage return on error

--- src/main/java/net/pandadev/nextron/utils/commandapi/node/CommandNode.java
+++ src/main/java/net/pandadev/nextron/utils/commandapi/node/CommandNode.java
@@ -2,6 +2,8 @@ package net.pandadev.nextron.utils.commandapi.node;
 
 import lombok.Getter;
 import lombok.SneakyThrows;
+import net.pandadev.nextron.Main;
+import net.pandadev.nextron.commands.HelpBase;
 import net.pandadev.nextron.utils.commandapi.Command;
 import net.pandadev.nextron.utils.commandapi.CommandHandler;
 import net.pandadev.nextron.utils.commandapi.bukkit.BukkitCommand;
@@ -9,7 +11,6 @@ import net.pandadev.nextron.utils.commandapi.help.HelpNode;
 import net.pandadev.nextron.utils.commandapi.paramter.Param;
 import net.pandadev.nextron.utils.commandapi.paramter.ParamProcessor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
@@ -25,42 +26,42 @@ public class CommandNode {
     @Getter
     private static final HashMap<Class<?>, Object> instances = new HashMap<>();
 
-    // Command information
+
     private final ArrayList<String> names = new ArrayList<>();
     private final String permission;
     private final String description;
     private final boolean async;
 
-    // Executor information
+
     private final boolean playerOnly;
     private final boolean consoleOnly;
 
-    // Reflect information
+
     private final Object parentClass;
     private final Method method;
 
-    // Arguments information
+
     private final List<ArgumentNode> parameters = new ArrayList<>();
 
-    // The help nodes associated with this node
+
     private final List<HelpNode> helpNodes = new ArrayList<>();
 
     public CommandNode(Object parentClass, Method method, Command command) {
-        // Loads names
+
         Arrays.stream(command.names()).forEach(name -> names.add(name.toLowerCase()));
 
-        // Retrieve information from annotation
+
         this.permission = command.permission();
         this.description = command.description();
         this.async = command.async();
         this.playerOnly = command.playerOnly();
         this.consoleOnly = command.consoleOnly();
 
-        // Reflection
+
         this.parentClass = parentClass;
         this.method = method;
 
-        // Register all the argument nodes
+
         Arrays.stream(method.getParameters()).forEach(parameter -> {
             Param param = parameter.getAnnotation(Param.class);
             if (param == null) return;
@@ -68,28 +69,22 @@ public class CommandNode {
             parameters.add(new ArgumentNode(param.name(), param.concated(), param.required(), param.defaultValue(), parameter));
         });
 
-        // Register bukkit command if it doesn't exist
+
         names.forEach(name -> {
             if (!BukkitCommand.getCommands().containsKey(name.split(" ")[0].toLowerCase()))
                 new BukkitCommand(name.split(" ")[0].toLowerCase());
         });
 
-        // Makes it so you can use /plugin:command
+
         List<String> toAdd = new ArrayList<>();
         names.forEach(name -> toAdd.add(CommandHandler.getPlugin().getName() + ":" + name.toLowerCase()));
         names.addAll(toAdd);
 
-        // Add node to array list
+
         nodes.add(this);
     }
 
-    /**
-     * Gets the probably that a player is referring to
-     * this command whenever executing a command
-     *
-     * @param args Arguments
-     * @return Match Probability
-     */
+
     public int getMatchProbability(CommandSender sender, String label, String[] args, boolean tabbed) {
         AtomicInteger probability = new AtomicInteger(0);
 
@@ -140,93 +135,75 @@ public class CommandNode {
         return probability.get();
     }
 
-    /**
-     * Sends a player the usage message of this command
-     */
+
     public void sendUsageMessage(CommandSender sender) {
         if (consoleOnly && sender instanceof Player) {
-            sender.sendMessage(ChatColor.RED + "This command can only be executed by console.");
+            sender.sendMessage(Main.getPrefix() + "§cThis command can only be executed by console.");
             return;
         }
 
         if (playerOnly && sender instanceof ConsoleCommandSender) {
-            sender.sendMessage(ChatColor.RED + "You must be a player to execute this command.");
+            sender.sendMessage(Main.getCommandInstance());
             return;
         }
 
         if (!permission.isEmpty() && !sender.hasPermission(permission)) {
-            sender.sendMessage(ChatColor.RED + "I'm sorry, although you do not have permission to execute this command.");
+            sender.sendMessage(Main.getPrefix() + "§cI'm sorry, although you do not have permission to execute this command.");
             return;
         }
 
-        StringBuilder builder = new StringBuilder(ChatColor.RED + "Usage: /" + names.get(0) + " ");
-        parameters.forEach(param -> {
-            if (param.isRequired())
-                builder.append("<").append(param.getName()).append(param.isConcated() ? ".." : "").append(">");
-            else builder.append("[").append(param.getName()).append(param.isConcated() ? ".." : "").append("]");
-            builder.append(" ");
-        });
-
-        // Sends the usage message
-        sender.sendMessage(builder.toString());
+        HashMap<String, ArrayList<String>> commandsList = new HashMap<String, ArrayList<String>>(HelpBase.commands);
+        String commandName = names.get(0);
+        sender.sendMessage(Main.getPrefix() + "§7Usage: \n§a" + commandsList.get(commandName).get(0));
     }
 
-    /**
-     * Gets the required arguments length
-     *
-     * @return Required Length
-     */
+
     public int requiredArgumentsLength() {
         int requiredArgumentsLength = names.get(0).split(" ").length - 1;
         for (ArgumentNode node : parameters) if (node.isRequired()) requiredArgumentsLength++;
         return requiredArgumentsLength;
     }
 
-    /**
-     * Executes the command
-     *
-     * @param sender Sender
-     * @param args   Arguments
-     */
+
     @SneakyThrows
     public void execute(CommandSender sender, String[] args) {
-        // Checks if the player has permission
+
         if (!permission.isEmpty() && !sender.hasPermission(permission)) {
-            sender.sendMessage(ChatColor.RED + "I'm sorry, although you do not have permission to execute this command.");
+            sender.sendMessage(Main.getPrefix() + "§cI'm sorry, although you do not have permission to execute this command.");
             return;
         }
 
-        // Checks if command is console only
+
         if (sender instanceof ConsoleCommandSender && playerOnly) {
-            sender.sendMessage(ChatColor.RED + "You must be a player to execute this command.");
+            sender.sendMessage(Main.getCommandInstance());
             return;
         }
 
-        // Checks if command is player only
+
         if (sender instanceof Player && consoleOnly) {
-            sender.sendMessage(ChatColor.RED + "This command is only executable by console.");
+            sender.sendMessage(Main.getPrefix() + "§cThis command can only be executed by console.");
             return;
         }
 
-        // Calculates the amount of arguments in the name
+
         int nameArgs = (names.get(0).split(" ").length - 1);
 
         List<Object> objects = new ArrayList<>(Collections.singletonList(sender));
         for (int i = 0; i < parameters.size(); i++) {
             ArgumentNode node = parameters.get(i);
             String suppliedArgument;
-            
-            // Check if the argument is marked as concatenated
+
+
             if (node.isConcated()) {
-                // Join all remaining arguments into a single string
+
                 suppliedArgument = String.join(" ", Arrays.copyOfRange(args, i + nameArgs, args.length));
-                // Process the concatenated argument
+
                 Object object = new ParamProcessor(node, suppliedArgument, sender).get();
-                if (object == null) return; // Handle parsing failure
+                if (object == null) return;
                 objects.add(object);
-                break; // No more arguments to process after a concatenated one
+                break;
             } else {
-                // Process a single argument
+
                 suppliedArgument = i < args.length - nameArgs ? args[i + nameArgs] : null;
                 if (suppliedArgument == null && node.isRequired()) {
                     sendUsageMessage(sender);
@@ -236,16 +213,16 @@ public class CommandNode {
                     if (!node.getDefaultValue().isEmpty()) {
                         suppliedArgument = node.getDefaultValue();
                     } else {
-                        continue; // Optional parameter not provided and no default value
+                        continue;
                     }
                 }
                 Object object = new ParamProcessor(node, suppliedArgument, sender).get();
-                if (object == null) return; // Handle parsing failure
+                if (object == null) return;
                 objects.add(object);
             }
         }
 
-        // Adjust the method invocation to match the dynamic size of objects
+
         Object[] methodArgs = convertParameters(method, objects);
         if (async) {
             Bukkit.getScheduler().runTaskAsynchronously(CommandHandler.getPlugin(), () -> {
@@ -265,14 +242,14 @@ public class CommandNode {
         Object[] convertedParams = new Object[parameterTypes.length];
         AtomicInteger index = new AtomicInteger(0);
 
-        // Iterate over the expected parameter types of the method
+
         for (int i = 0; i < parameterTypes.length; i++) {
-            // If there's a corresponding original parameter, use it
+
             if (i < originalParams.size() && originalParams.get(i) != null) {
                 convertedParams[i] = originalParams.get(i);
             } else {
-                // For missing or null original parameters, check if a default value is needed
-                // For simplicity, we're just using null here
+
+
                 convertedParams[i] = null;
             }
         }
