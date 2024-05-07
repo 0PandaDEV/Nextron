8950e36331fc89f506a788d5a1ee483b1cd1c95d
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-05-07 19:27:00 +0200

    added ping command

---
+++ src/main/java/net/pandadev/nextron/commands/PingCommand.java
@@ -0,0 +1,17 @@
+package net.pandadev.nextron.commands;
+
+import net.pandadev.nextron.Main;
+import net.pandadev.nextron.utils.commandapi.Command;
+import org.bukkit.entity.Player;
+
+public class PingCommand extends HelpBase {
+
+    public PingCommand() {
+        super("ping, Returns your current ping ot the server, /ping");
+    }
+
+    @Command(names = {"ping"}, permission = "nextron.ping", playerOnly = true)
+    public void pingCommand(Player player) {
+        player.sendMessage(Main.getPrefix() + "§7Your ping is §a" + player.getPing());
+    }
+}
