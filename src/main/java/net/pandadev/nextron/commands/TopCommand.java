55afb5d5f53edda14c443931998f0762ecf6c265
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-07-15 15:05:27 +0200

    :)

--- src/main/java/net/pandadev/nextron/commands/TopCommand.java
+++ src/main/java/net/pandadev/nextron/commands/TopCommand.java
@@ -10,7 +10,7 @@ import net.pandadev.nextron.utils.Configs;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-@Command(name = "top")
+@Command(name = "top", aliases = {"touchgrass"})
 @Permission("nextron.top")
 public class TopCommand extends HelpBase {
 
