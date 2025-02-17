8fb6c7c581c5f14c7d9e7c1dbad43dd0fbc9f738
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2025-02-17 17:22:50 +0100

    fix: invsee command not responding at all

--- src/main/java/net/pandadev/nextron/commands/InvseeCommand.java
+++ src/main/java/net/pandadev/nextron/commands/InvseeCommand.java
@@ -3,6 +3,7 @@ package net.pandadev.nextron.commands;
 import dev.rollczi.litecommands.annotations.argument.Arg;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
+import dev.rollczi.litecommands.annotations.execute.Execute;
 import dev.rollczi.litecommands.annotations.permission.Permission;
 import net.pandadev.nextron.Main;
 import net.pandadev.nextron.languages.TextAPI;
@@ -16,6 +17,7 @@ public class InvseeCommand extends HelpBase {
         super("invsee, Lets you inspect and control another player's inventory, /invsee <player>");
     }
 
+    @Execute
     public void invseeCommand(@Context Player player, @Arg Player target) {
         if (target != player) {
             player.openInventory(target.getInventory());
